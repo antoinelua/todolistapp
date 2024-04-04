431aee6f39493d671e6a7aec1976e22436e174e5
antoinelua <antoinelua10@gmail.com>
2024-04-04 16:35:15 -0700

    My first todolistapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoForm } from './components/TodoForm';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const TodoForm = () => {
+    return (
+        <form className='TodoForm'>
+            <input type="text" className='todo-input'
+                placeholder='What is the task today?' />
+            <button type='submit' className='todo-btn' />
+        </form>
+    );
+};
